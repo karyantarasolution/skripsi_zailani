c5b1c6942d7e0cba550c7e516ee7b66968f9d2cc
Karyantara Solution <karyantarasolution@gmail.com>
2026-08-20 10:45:45 +0800

    revisi 23

--- resources/views/admin/bahan/index.blade.php
+++ resources/views/admin/bahan/index.blade.php
@@ -20,10 +20,7 @@
             <div class="flex flex-wrap gap-4 mb-6 justify-between items-center">
                 <div class="bg-indigo-950 px-6 py-3 rounded-2xl text-white shadow-lg flex items-center gap-3">
                     <svg class="w-6 h-6 text-indigo-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm14 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"/></svg>
-                    <div>
-                        <p class="text-[10px] font-black uppercase tracking-widest text-indigo-300">Mode Barcode</p>
-                        <p class="text-sm font-bold">Siap Scan Barang Masuk</p>
-                    </div>
+                  
                 </div>
                 <button @click="modalAdd = true" class="px-6 py-4 bg-indigo-600 hover:bg-indigo-500 text-white font-black uppercase text-xs tracking-widest rounded-2xl shadow-xl transition transform active:scale-95">
                     + Register Barang Baru
